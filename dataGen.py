b172587e24cc519c69b2920062c334186c96f2f8
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-11-10 13:14:49 -0500

    Update dataGen.py

--- dataGen.py
+++ dataGen.py
@@ -88,7 +88,7 @@ for _ in range(1000):
     is_superuser = random.choice([True, False])
     first_name = fake.first_name()
     last_name = fake.last_name()
-    username = first_name[0] + last_name + randint(100, 999)
+    username = first_name[0] + last_name + str(randint(100, 999))
     email = username.lower() + '@example.com'  # Email starts with the username
     is_staff = random.choice([True, False])
     is_active = random.choice([True, False])
